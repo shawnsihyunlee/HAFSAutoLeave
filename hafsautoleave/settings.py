7fc3934a861105ae52b33299fbcb249eaf400a57
Shawn <shawn4jworld@gmail.com>
2019-08-02 23:54:28 +0900

    Add javascript

--- hafsautoleave/settings.py
+++ hafsautoleave/settings.py
@@ -126,6 +126,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
---
+++ main/static/js/init.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+//     $('.sidenav').sidenav();
+//   });
+
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.modal').modal();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
