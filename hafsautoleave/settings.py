66637815b4e07de32025df299a1a9f1aa6b1a4a2
Shawn <shawn4jworld@gmail.com>
2019-08-10 10:09:03 +0900

    Added django-heroku

--- hafsautoleave/settings.py
+++ hafsautoleave/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,3 +127,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
+
+django_heroku.settings(locals())
