793ce20f6b30863533a2477ed7ad4b822d3e0d25
Shawn <shawn4jworld@gmail.com>
2019-08-10 14:25:24 +0900

    added exception handling

--- irregularleavescript.py
+++ irregularleavescript.py
@@ -54,55 +54,57 @@ def doSignup():
             
             # 로그인 URL
             url = "http://going.hafs.hs.kr/login/login_process.php"
-            
-            # Initiate login
-            r = s.post(url, data = login_data)
-            
-            if ("alert" in str(r.content)):
-                print("Wrong Login Data")
-                raise Exception("Wrong Login Data")
-            
-            # Logged in
-            r = s.get('http://going.hafs.hs.kr/lod/out_list_student_h.php')
-            
-            
-            # 비정기 외출 신청 get request
-            r = s.get("http://going.hafs.hs.kr/lod/out_reg2_student_h.php")
-            r.encoding = 'euc-kr'
-            
-            # 외출 정보
-            soup = BeautifulSoup(r.content, 'html.parser')
-            student_seq = soup.find('input', attrs = {'name' : 'student_seq'})["value"]  # Find student sequence number
-            
-            out_date = datedict[info.out_day.lower()]
-            out_hour = str(info.out_hour)
-            out_minute = str(info.out_minute)
-            return_date = datedict[info.return_day.lower()]
-            return_hour = str(info.return_hour)
-            return_minute = str(info.return_minute)
+            try:
+                # Initiate login
+                r = s.post(url, data = login_data)
+                
+                if ("alert" in str(r.content)):
+                    print("Wrong Login Data")
+                    raise Exception("Wrong Login Data")
+                
+                # Logged in
+                r = s.get('http://going.hafs.hs.kr/lod/out_list_student_h.php')
+                
+                
+                # 비정기 외출 신청 get request
+                r = s.get("http://going.hafs.hs.kr/lod/out_reg2_student_h.php")
+                r.encoding = 'euc-kr'
+                
+                # 외출 정보
+                soup = BeautifulSoup(r.content, 'html.parser')
+                student_seq = soup.find('input', attrs = {'name' : 'student_seq'})["value"]  # Find student sequence number
+                
+                out_date = datedict[info.out_day.lower()]
+                out_hour = str(info.out_hour)
+                out_minute = str(info.out_minute)
+                return_date = datedict[info.return_day.lower()]
+                return_hour = str(info.return_hour)
+                return_minute = str(info.return_minute)
 
-            if(out_minute == "0"):
-                out_minute = "00"
-            if(return_minute == "0"):
-                return_minute = "00"
-            
-            leave_time_data = {
-                "student_seq" : student_seq,
-                "search_YN" : 'Y',
-                "base_seq" : '',
-                "out_date" : out_date,
-                "out_hour" : out_hour,
-                "out_minute" : out_minute,
-                "return_date" : return_date,
-                "return_hour" : return_hour,
-                "return_minute" : return_minute,
-                "bigo" : '',
-                "count" : ''
-            }
+                if(out_minute == "0"):
+                    out_minute = "00"
+                if(return_minute == "0"):
+                    return_minute = "00"
+                
+                leave_time_data = {
+                    "student_seq" : student_seq,
+                    "search_YN" : 'Y',
+                    "base_seq" : '',
+                    "out_date" : out_date,
+                    "out_hour" : out_hour,
+                    "out_minute" : out_minute,
+                    "return_date" : return_date,
+                    "return_hour" : return_hour,
+                    "return_minute" : return_minute,
+                    "bigo" : '',
+                    "count" : ''
+                }
 
-            print(leave_time_data) #debugging
-            
-            r = s.post("http://going.hafs.hs.kr/lod/out_reg2_student.php", data = leave_time_data)
-            r.encoding = 'euc-kr'
-            #print("Status:" , r.status_code)
-            print(r.text)
+                print(leave_time_data) #debugging
+                
+                r = s.post("http://going.hafs.hs.kr/lod/out_reg2_student.php", data = leave_time_data)
+                r.encoding = 'euc-kr'
+                #print("Status:" , r.status_code)
+                print(r.text)
+            except Exception as e:
+                print(e)
