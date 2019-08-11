125047c44cadab440e54819dbb4ae15dd407324b
Shawn <shawn4jworld@gmail.com>
2019-08-11 18:15:46 +0900

    added cron job on monday

--- clock.py
+++ clock.py
@@ -8,17 +8,26 @@ q = Queue(connection=conn)
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='sun', hour=18, minute=13, timezone="Asia/Seoul")
+@sched.scheduled_job('cron', day_of_week='mon', hour=23, minute=00, timezone="Asia/Seoul")
 def scheduled_job():
 	#print("Test: Monday")
 	autoleavescript.doSignup()
 	#q.enqueue(test)
-    #q.enqueue(irregularleavescript.doSignup)
+    #q.enqueue(autoleavescript.doSignup)
 
 # def test():
 # 	print("This job is run by the worker every minute.")
 
 sched.start()
+
+
+
+
+
+
+
+
+
 
 
 
