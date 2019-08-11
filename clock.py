a37f955a8cc8728c7ba235edf1fe07c3de2f0a17
Shawn <shawn4jworld@gmail.com>
2019-08-11 18:10:51 +0900

    cron testing

--- clock.py
+++ clock.py
@@ -8,7 +8,7 @@ q = Queue(connection=conn)
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='mon', hour=18, minute=9, timezone="Asia/Seoul")
+@sched.scheduled_job('cron', day_of_week='sun', hour=18, minute=13, timezone="Asia/Seoul")
 def scheduled_job():
 	#print("Test: Monday")
 	autoleavescript.doSignup()
